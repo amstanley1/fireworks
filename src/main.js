341c461ed074748a0c26396c25aa6ef0577785dd
Ashley Stanley <amstnly@gmail.com>
2018-12-17 14:31:21 -0500

    add buttons

--- src/main.js
+++ src/main.js
@@ -8,4 +8,9 @@ export default () => {
   const container = document.getElementById('container');
   const fireworks = new Fireworks(container);
   fireworks.start();
+
+  const startButton = document.getElementById('#startButton');
+  const stopButton = document.getElementById('#stopButton');
+  startButton.addEventListener("click",function(){fireworks.start()});
+  stopButton.addEventListener("click",function(){fireworks.stop()});
 };
